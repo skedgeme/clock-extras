adb68e7823391d574e929e1cdeaba226d97d384c
Jonathan Fischoff <jonathan.fischoff@skedge.me>
2016-10-04 15:28:00 -0400

    loosen clock deps

--- src/System/Clock/TimeIt.hs
+++ src/System/Clock/TimeIt.hs
@@ -1,15 +1,24 @@
-{-| A couple functions that probably should be in the 'clock' package. 
+{-# LANGUAGE CPP, ViewPatterns #-}
+{-| A couple functions that probably should be in the 'clock' package.
     See this issue: https://github.com/corsis/clock/issues/42
 -}
 module System.Clock.TimeIt where
 import System.Clock
 
+#if !MIN_VERSION_base(7,0,0)
+-- This code is ripped out of System.Clock 0.7.1
+s2ns :: Num a => a
+s2ns = 10^(9 :: Int)
+
+toNanoSecs :: TimeSpec -> Integer
+toNanoSecs   (TimeSpec  (toInteger -> s) (toInteger -> n)) = s * s2ns + n
+#endif
 -- | Time an action. Return the elasped time the result
 elapsedTime :: IO a -> IO (a, Double)
 elapsedTime action = do
-  startTime <- getTime Monotonic 
+  startTime <- getTime Monotonic
   r <- action
-  endTime   <- getTime Monotonic 
+  endTime   <- getTime Monotonic
 
   let result = diffSeconds endTime startTime
 
